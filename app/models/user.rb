d40072f7ec9ce0f0ce10bd44527f2840b3b23c9a
lexus2727 <lexus2424@yahoo.com>
2019-10-29 10:26:56 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    validates :name, presence: true
+  
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  before_create :cleanup_string_input
+
+  def mood
+    self.happiness > self.nausea ? "happy" : "sad"
+  end
+
+
+  def cleanup_string_input
+    self.happiness = self.happiness.to_i
+    self.nausea = self.nausea.to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+
+  resources :rides
+
+  resources :attractions
+
+  resources :users, :sessions
+
+  root 'static_pages#home'
+
+  get 'signin', to: 'sessions#new'
+
+  get '/signout', to: 'sessions#destroy'
+  post "/rides/new", to: "rides#new"
+
