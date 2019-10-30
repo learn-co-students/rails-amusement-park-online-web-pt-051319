e4dc641078a9cbdbb47ab871e3f91b71b17c1e2f
Jen <jenniferanndcb@gmail.com>
2019-10-30 16:46:08 +0000

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,42 @@
+class AttractionsController < ApplicationController 
+
+  before_action :set_attraction, only: [:show, :edit, :update, :destroy]
+
+  def index 
+    @attractions = Attraction.all
+  end 
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  def destroy
+    @attraction.delete
+  end
+
+  private
+
+  def set_attraction
+      @attraction = Attraction.find(params[:id])
+  end
+
+  def attraction_params
+      params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
+  resources :attractions
+  resources :rides, only: [:new, :create]
+
+  #sessions
+  get '/signin', to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+
+  root "welcome#home"
+
 end
